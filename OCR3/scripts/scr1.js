a6e345a8c456b4b936885f08b3b89aa13a34edd8
VladVrons <slava.pro1605@gmail.com>
2020-11-03 19:01:08 +0200

    ColorPick
    Colorpick and local storage are working

--- OCR3/scripts/scr1.js
+++ OCR3/scripts/scr1.js
@@ -1,10 +1,25 @@
+window.onload = function() {
+    alert('Страница загружена');
+    
+    maintext.style.color=localStorage.getItem("color");
+    // к этому моменту страница загружена
+  };
+
 
 a=5;
 h=7;
-S=5*7*0.5; 
+S=5*7*0.5;
+
+
+Stick=true;
 function Clicked2()
 {
-document.getElementById("maintext").innerHTML=document.getElementById("maintext").innerHTML+"S="+S;
+    if(Stick)
+    {
+        document.getElementById("maintext").innerHTML=document.getElementById("maintext").innerHTML+"S="+S;
+        Stick=false;
+    }
+
 }
 
 function Clicked1()
@@ -12,6 +27,15 @@ function Clicked1()
     r=document.getElementById("rtext").innerText;
     document.getElementById("rtext").innerText=document.getElementById("ltext").innerText;
     document.getElementById("ltext").innerText=r;
+}
+
+function Clicked3 ()
+{
+    var input1 =  document.getElementById("colorpick");
+    maintext.style.color=input1.value;
+    
+    localStorage.setItem("color",input1.value);
+    //document.getElementById("colorpick").value="ff0000";
 }
 
 
