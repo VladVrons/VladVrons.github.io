6112015aa6d2a325daea22a2fee99d998dea16fe
VladVrons <slava.pro1605@gmail.com>
2020-12-03 14:39:23 +0200

    Ball move
    Ball moving and bouncing off walls

--- OCR5/scripts/index.js
+++ OCR5/scripts/index.js
@@ -4,6 +4,21 @@ function Task1()
   document.getElementById("popup_text").innerText="Hi what's up!";
 }
 
+function Start()
+{
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+setInterval(draw, 5);
+}
+
+
+
+
 
 function Close()
 {
