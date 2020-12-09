c6ee583a7cd50ab3bcc3d49d85d8b89946cca208
VladVrons <slava.pro1605@gmail.com>
2020-12-09 20:45:19 +0200

    Stop and start

--- OCR5/scripts/animation.js
+++ OCR5/scripts/animation.js
@@ -12,22 +12,40 @@ function getRadians(degrees)
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
+var x = ballRadius
+var y = ballRadius + getRandomInt(canvas.height-30)
+var x2 = ballRadius+ getRandomInt(canvas.width);
+var y2 = ballRadius;
+
 var dx = 1;
 var dy = -1;
 
-function drawBall() {
+var dx2 = 1;
+var dy2 = -1;
+let copyd=dx;
+
+
+function drawBall1() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "green";
     ctx.fill();
     ctx.closePath();
 }
 
+function drawBall2() {
+    ctx.beginPath();
+    ctx.arc(x2, y2, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
+
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
+    drawBall1();
+    drawBall2();
     
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -38,6 +56,32 @@ function draw() {
     
     x += dx;
     y += dy;
+  
+
+    if(x2 + dx2 > canvas.width-ballRadius || x2 + dx2 < ballRadius) {
+        dx2 = -dx2;
+    }
+    if(y2 + dy2 > canvas.height-ballRadius || y2 + dy2 < ballRadius) {
+        dy2 = -dy2;
+    }
+    x2 += dx2;
+    y2 += dy2;
+
+
+
+
+
+    if( ( Math.abs((x+dx) - (x2+dx2)) < ballRadius*1.5) && ( Math.abs((y+dy) - (y2+dy2)) < ballRadius*1.5) )
+    {
+        dx=0;
+        dy=0;
+        dx2=0;
+        dy2=0;
+        document.getElementById("startb").innerHTML="Reload";
+    }
+
+
+    
 }
 
 
--- OCR5/scripts/index.js
+++ OCR5/scripts/index.js
@@ -4,13 +4,41 @@ function Task1()
   document.getElementById("popup_text").innerText="Hi what's up!";
 }
 
+
+let startf=false;
+let f1=true;
+
+let cdx=1;
+let cdy=-1;
+
+let cdx2=1;
+let cdy2=-1;
+
 function Start()
 {
+  if(document.getElementById("startb").innerHTML=="Reload")
+  {
+     x = ballRadius
+     y = ballRadius + getRandomInt(canvas.height-30)
+     x2 = ballRadius+ getRandomInt(canvas.width);
+     y2 = ballRadius;
+  }
 
-setInterval(draw, 5);
+
+  document.getElementById("startb").innerHTML="Pause"
+  dx=cdx; dy=cdy; dx2=cdx2; dy2=cdy2;
+  startf= !startf;
+  document.getElementById("startb").value="pause";
+
+  if(f1) {var mytimer=setInterval(draw, 5); f1=false}
+  if(startf==false){ 
+    let cdx=dx;
+    let cdy=dy;
+    let cdx2=dx2;
+    let cdy2=dy2; 
+    document.getElementById("startb").innerHTML="Resume"
+    dx=0; dy=0; dx2=0; dy2=0;}
 }
-
-
 
 
 
